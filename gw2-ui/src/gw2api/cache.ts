c02d99072a83ce4d2048df4a6aac38039d5374d1
gw2princeps <21026467+gw2princeps@users.noreply.github.com>
2024-07-27 18:04:30 +0200

    fix: leftover code for multiple api routes

--- gw2-ui/src/gw2api/cache.ts
+++ gw2-ui/src/gw2api/cache.ts
@@ -228,6 +228,7 @@ export default class APICache<T extends { id: Id }> {
         '&lang=' +
         this.language;
       const res = await fetch(url, FETCH_OPTIONS);
+
       if (res.status === 404) {
         // 404 usually means that none of the passed ids are known, which is equivalent to an empty response
         response = [];
