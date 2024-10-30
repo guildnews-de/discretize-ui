5314858655fcdc6ad65bf73126bae8bf15b36023
Marcus <marcustyphoon@gmail.com>
2024-10-30 00:33:09 -0700

    remove typescript-is workaround

--- gw2-ui/src/gw2api/types/common/traited_fact.ts
+++ gw2-ui/src/gw2api/types/common/traited_fact.ts
@@ -1,25 +1,4 @@
-import {
-  type GW2ApiFactAttributeAdjust,
-  type GW2ApiFactBuff,
-  type GW2ApiFactBuffConversion,
-  type GW2ApiFactComboField,
-  type GW2ApiFactComboFinisher,
-  type GW2ApiFactDamage,
-  type GW2ApiFactDistance,
-  type GW2ApiFactDuration,
-  type GW2ApiFactHeal,
-  type GW2ApiFactHealingAdjust,
-  type GW2ApiFactNoData,
-  type GW2ApiFactNumber,
-  type GW2ApiFactPercent,
-  type GW2ApiFactPrefixedBuff,
-  type GW2ApiFactRadius,
-  type GW2ApiFactRange,
-  type GW2ApiFactRecharge,
-  type GW2ApiFactStunBreak,
-  type GW2ApiFactTime,
-  type GW2ApiFactUnblockable,
-} from './fact';
+import { type GW2ApiFact } from './fact';
 
 type GW2ApiTraitedFactAdditional = {
   requires_trait: number;
